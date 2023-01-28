cad6c406150ec4f9c7023123b25db9806998207b
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-01-28 23:05:34 +0700

    Time: 251 ms (58.17%), Space: 78.5 MB (85.53%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> temp; // position - speed
+        int n = position.size();
+        for(int i = 0; i<n; i++) temp.emplace_back(position[i],speed[i]);
+        sort(temp.begin(), temp.end(), greater<pair<int,int>>()); // sort in decreasing order
+        stack<pair<int,int>> s;
+        s.emplace(temp[0]);
+        for(int i = 1; i<n; i++){
+            float time_next_car = (target - s.top().first)*1.0/(s.top().second);
+            float time_curr_car = (target - temp[i].first)*1.0/(temp[i].second);
+            if(time_curr_car > time_next_car){
+                s.emplace(temp[i]);
+            }
+        }
+        return s.size();
+    }
+};
