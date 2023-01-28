c78bd24de55d128ae8c2580653aca518f732c266
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-01-28 23:07:30 +0700

    Time: 415 ms (30.74%), Space: 78.6 MB (66.97%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -6,12 +6,12 @@ public:
         for(int i = 0; i<n; i++) temp.emplace_back(position[i],speed[i]);
         sort(temp.begin(), temp.end(), greater<pair<int,int>>()); // sort in decreasing order
         stack<pair<int,int>> s;
-        s.emplace(temp[0]);
+        s.push(temp[0]);
         for(int i = 1; i<n; i++){
             float time_next_car = (target - s.top().first)*1.0/(s.top().second);
             float time_curr_car = (target - temp[i].first)*1.0/(temp[i].second);
             if(time_curr_car > time_next_car){
-                s.emplace(temp[i]);
+                s.push(temp[i]);
             }
         }
         return s.size();
