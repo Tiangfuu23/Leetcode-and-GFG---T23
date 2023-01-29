8a658c29e0e3078d70a036bc4dedf450431396b2
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-01-29 17:23:22 +0700

    Time: 0 ms (100.00%), Space: 6 MB (7.52%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -9,11 +9,11 @@ public:
         while(l<r){
             int mid = l + (r-l)/2;
             if(isBadVersion(mid)){
-                r = mid-1;
+                r = mid;
             }else{
                 l = mid+1;
             }
         }
-        return isBadVersion(l) ? l : l+1;
+        return l;
     }
 };
