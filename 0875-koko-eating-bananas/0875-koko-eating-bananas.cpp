eeb0ea323c6c52a052d562677682c0c0fab58e9a
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-01-29 20:06:05 +0700

    Time: 40 ms (94.33%), Space: 35.1 MB (6.36%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,24 +3,24 @@
 class Solution {
 public:
     bool valid(vector<int> piles, int h, int num){
-        int cnt = 0;
+        long long cnt = 0;
         for(int i = 0; i<piles.size(); i++){
             cnt += ceil(piles[i]*1.0/num);
         }
         return cnt <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max = -1;   
-        for(int& i : piles) if(i>max) max = i;
-        int l = 1, r = max;
-        while(l<r){
+        int max = *max_element(piles.begin(),piles.end());
+        int l = 1, r = max, res = max;
+        while(l<=r){
             int mid = l+(r-l)/2;
             if(valid(piles,h,mid)){
-                r = mid;
+                res = mid;
+                r = mid-1;
             }else{
                 l = mid+1;
             }
         } 
-    return l;
+    return res;
     }
 };
