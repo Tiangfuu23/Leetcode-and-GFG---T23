bae7bd88dee1b9ffa445a6858efffbbb109afa65
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-01 18:19:21 +0700

    Added solution - LeetHub

---
+++ Binary Search - GFG/binary-search.js
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(
+        string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res, n) {
+    let s = "";
+    for (let i = 0; i < n; i++) {
+        s += res[i];
+        s += " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        for (let i = 0; i < n; i++) {
+            arr[i] = input_ar1[i];
+        }
+        let k = parseInt(readLine());
+        let obj = new Solution();
+        let res = obj.binarysearch(arr, n, k);
+        console.log(res);
+    }
+} // } Driver Code Ends
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+*/
+
+class Solution {
+    binarysearch(arr, n, k) {
+        // code here
+        let l = 0, r = arr.length -1;
+        while(l<=r){
+            let mid = l+parseInt((r-l)/2);
+            if(arr[mid]===k) return mid;
+            else if(arr[mid]>k) r = mid-1;
+            else l = mid+1;
+        }
+    return -1;
+    }
+}
