dbba58e9f368a1f6586d91b9a3de3e04fe02ffab
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-05 00:54:39 +0700

    Time: 12 ms (36.98%), Space: 13.5 MB (17.45%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,39 @@
+// same as count lever in tree pattern
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size();
+        int length = 0, fresh = 0;
+        queue<pair<int,int>> q;
+        for(int i =0 ; i<row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2) {
+                    q.emplace(i,j);
+                    grid[i][j] = -2;
+                }
+            }
+        }
+        if(fresh == 0) return 0; // edge case
+        while(!q.empty()){
+            int n = q.size();
+            for(int i =0; i<n; i++){
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+                vector<pair<int,int>> neighbours = {{0,-1},{-1,0},{1,0},{0,1}};
+                for(auto i : neighbours){
+                    int new_r = r + i.first, new_c = c + i.second;
+                    if(new_r<0 || new_c<0 || new_r>=row || new_c>=col || grid[new_r][new_c] == -2 || grid[new_r][new_c] == 0) continue;
+                    // now grid[new_r][new_c] = 1
+                        grid[new_r][new_c] = -2;
+                        fresh--;
+                        q.emplace(new_r,new_c);
+                }
+            }
+            length++;
+        }
+        if(fresh != 0) return -1;
+        return length-1;
+    }
+};
