5844eef61ce7a9847e1b95f88ccda05537e75d38
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-04 13:18:56 +0700

    Added solution - LeetHub

---
+++ Largest Element in Array - GFG/largest-element-in-array.js
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.largest(arr, n);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+class Solution {
+    largest(arr, n) {
+       let res = Number.MIN_SAFE_INTEGER;
+       for(let i = 0; i<n;  i++) if(arr[i] > res) res = arr[i];
+       return res;
+    }
+}
