4c17cf3e58e9f348b23590015aab5afa213b46c7
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-04 13:15:00 +0700

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.js
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        
+        let [n,x] = readLine().trim().split(" ").map((x) => parseInt(x));
+        let arr=readLine().trim().split(" ").map((x) => parseInt(x));
+        let obj = new Solution();
+        let res = obj.find(arr,n,x);
+        let s = "";
+        for(let it of res){
+            s+=it+" ";
+        }
+        console.log(s);
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} x
+ * @return {number[]}
+*/
+
+class Solution {
+    find(arr,n,x){
+       let [first,last] = [-1,-1];
+       for(let i =0 ; i<n; i++){
+           if(first === -1 && arr[i] == x) first = i;
+           if(arr[i] === x) last = i;
+       }
+       return [first,last];
+    }
+}
