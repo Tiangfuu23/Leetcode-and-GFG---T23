37c4958f84088045326f56323de7a8f4f6ed3f6e
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-12 20:53:34 +0700

    Time: 199 ms (93.57%), Space: 107.4 MB (14.92%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,21 @@
+// same as find next greater number 
+// using stack time O(n) space O(n)
 class Solution {
-public:
+public: 
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> res(n,0);
-        stack<pair<int,int>> s; // temp - index
-        for(int i = 0; i<n; i++){
-            while(!s.empty() && temperatures[i] > s.top().first){
-                res[s.top().second] = i - s.top().second;
+        stack<pair<int,int>> s; // value - index 
+        for(int i = 0; i<temperatures.size(); i++){
+            int curr_number = temperatures[i];
+            while(!s.empty() && curr_number > s.top().first){
+                temperatures[s.top().second] = i-s.top().second;
                 s.pop();
             }
-            s.push({temperatures[i],i});
+            s.push({curr_number,i});
         }
-        return res;
+        while(!s.empty()){
+            temperatures[s.top().second] = 0;
+            s.pop();
+        }
+    return temperatures;
     }
 };
