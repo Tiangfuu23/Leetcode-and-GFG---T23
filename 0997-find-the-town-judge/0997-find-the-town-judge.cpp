8fb004726e928f26005279aede366202b72f7f11
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-21 01:42:34 +0700

    Time: 238 ms (44.42%), Space: 70.5 MB (13.55%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==1) return 1;
+        unordered_set<int> trustSomeOne;
+        unordered_map<int,int> count;
+        for(auto i : trust){
+            trustSomeOne.insert(i[0]);
+            if(count.find(i[1]) != count.end()) count[i[1]]++;
+            else count.insert({i[1],1});
+        }
+        for(int i = 1; i<=n; i++){
+            if(trustSomeOne.find(i) == trustSomeOne.end()){
+                if(count.find(i) != count.end() && count[i] == n-1) return i;
+            }
+        }
+        return -1;
+    }
+};
