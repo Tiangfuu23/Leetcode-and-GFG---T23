a0223426eefb3219fa42d1d25eb0e1786b116fff
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-21 21:03:36 +0700

    Time: 18 ms (79.74%), Space: 8.8 MB (93.30%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         typedef tuple<int,int,int> tp; // height - rol - col
+        vector<pair<int,int>> choices {{0,-1},{-1,0},{1,0},{0,1}};
         int n = grid.size();
         vector<vector<bool>> visited(n, vector<bool> (n,false));
         priority_queue<tp, vector<tp>, greater<tp> > pq;
@@ -11,7 +12,6 @@ public:
             int h,row,col;
             tie(h,row,col) = pq.top(); pq.pop();
             if(row==n-1 && col==n-1) return h;
-            vector<pair<int,int>> choices {{0,-1},{-1,0},{1,0},{0,1}};
             for(pair<int,int>& i : choices){
                 int new_rol = row + i.first, new_col = col + i.second;
                 if(new_rol < 0 || new_rol >= n || new_col < 0 || new_col >= n || visited[new_rol][new_col]) continue;
