aca1c2540db085ca4fc61cd8eb56a6b54d0846c3
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-02-24 00:03:06 +0700

    Time: 8 ms (15.53%), Space: 7.9 MB (51.27%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int row = obstacleGrid.size(), col = obstacleGrid[0].size();
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[row-1][col-1] == 1) return 0;
+        using ll = long long;
+        vector<vector<ll>> grid (row, vector<ll> (col,0));
+        grid[0][0] = grid[row-1][col-1] = 1;
+        for(int i = col-2; i>=0; i--){
+            if(obstacleGrid[row-1][i] == 1){
+                grid[row-1][i] = 0;
+            }else{
+                grid[row-1][i] = grid[row-1][i+1];
+            }
+        }
+        for(int i = row-2; i>=0; i--){
+            if(obstacleGrid[i][col-1] == 1){
+                grid[i][col-1] = 0;   
+            }else{
+                grid[i][col-1] = grid[i+1][col-1];
+            }
+        }
+        for(int i = row-2; i>=0; i--){
+            for(int j = col-2; j>=0; j--){
+                if(obstacleGrid[i][j] == 1) {
+                    grid[i][j] = 0;
+                }else{
+                    grid[i][j] = grid[i+1][j] + grid[i][j+1];
+                }
+            }
+        }
+        return grid[0][0];
+    }
+};
