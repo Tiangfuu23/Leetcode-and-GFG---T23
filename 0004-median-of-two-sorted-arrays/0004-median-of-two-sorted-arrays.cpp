b4f40cb447447954a404731ba539adb3c3b7df42
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-04 01:33:36 +0700

    Time: 44 ms (59.45%), Space: 89.3 MB (75.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,19 +1,47 @@
+/*
+    partitionX + partitionY = (x + y )/2
+    found:
+        maxLeftX <= maxRight Y
+        maxLeftY <= maxRight X
+    else if
+        maxLeftX > maxRightY then move toward left in x 
+    else 
+        move toward right in X
+*/
 class Solution {
-public: //O(n) solution
+public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<int> sorted(n+m,0);
-        int l = 0, r = 0, k = 0;
-        while(l<n && r <m) sorted[k++] = nums1[l] < nums2[r] ? nums1[l++] : nums2[r++];
-        while(l<n) sorted[k++] = nums1[l++];
-        while(r<m) sorted[k++] = nums2[r++];
-        double ans = 0;
-        if((n+m)%2 != 0){
-            ans = sorted[(n+m)/2];
-        }else{
-            ans = sorted[(n+m)/2 -1] + sorted[(n+m)/2];
-            ans /= 2;
+        if(n==0){
+            if(m%2!=0) return nums2[m/2];
+            else return (nums2[m/2-1]*1.0+nums2[m/2])/2;
         }
-        return ans;
+        if(n > m) return findMedianSortedArrays(nums2,nums1);
+        int l = -1, r = n;
+        while(l<=r){
+            int partitionX = (l+r)/2 ;
+            // number of element in partX partitionX + 1;
+            // no of elements in parY = (n+m)/2 - partitionX - 1
+            int partitionY = (n+m)/2-partitionX - 2;
+
+            double maxLeftX = partitionX == -1 ? INT_MIN : nums1[partitionX];
+            double minRightX = partitionX == n-1 ? INT_MAX : nums1[partitionX+1];
+
+            double maxLeftY = partitionY == -1 ? INT_MIN : nums2[partitionY];
+            double minRightY = partitionY == m-1 ? INT_MAX : nums2[partitionY+1];
+            
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if((n+m)%2 != 0){
+                    return fmin(minRightX,minRightY);
+                }else{
+                    return (fmax(maxLeftX,maxLeftY) + fmin(minRightX,minRightY))/2; 
+                }
+            }else if(maxLeftX > minRightY){
+                r = partitionX -1;
+            }else{
+                l = partitionX + 1;
+            }
+        }
+    return 0;
     }
 };
