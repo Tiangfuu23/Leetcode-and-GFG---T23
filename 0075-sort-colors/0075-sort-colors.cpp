b382d995db74b308f16842176f1281dd3b0aadd1
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-15 23:38:36 +0700

    Time: 6 ms (23.60%), Space: 8.4 MB (31.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int h[3] ={0}; // frequencies of i = 0,1,2
-        for(int i : nums) h[i]++;
-        int j = 0;
-        for(int i = 0; i<=2; i++){
-            while(h[i]!=0){
-                nums[j] = i;
-                h[i]--;
-                j++;
+        int l = 0, r = nums.size()-1, i = 0;
+        while(i<=r){
+            if(nums[i] == 0){
+                swap(nums[l],nums[i]);
+                l++;
+                i++;
+            }else if(nums[i] == 2){
+                swap(nums[r],nums[i]);
+                r--;
+            }else{
+                i++;
             }
         }
     }
