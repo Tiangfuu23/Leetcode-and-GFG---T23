3c1a871ade5fe78767726d2f9f0620613a7acf79
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-16 13:18:07 +0700

    Time: 114 ms (13.28%), Space: 52.5 MB (34.56%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,28 +1,14 @@
-// time O(n) space O(1) solution
-// not that ans belongs to [1,.....len(nums) + 1]
-class Solution {
-public:
-    bool isSafe(int i, int n){
-        return i<=n && i>=1;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    nums.sort((a,b)=>a-b);
+    let ans = 1;
+    for(let i of nums){
+        if(i<=0) continue;
+        else if(i == ans) ans++
+        else if(i>ans) return ans;
     }
-    int firstMissingPositive(vector<int>& nums) {
-        int n =  nums.size();
-        // get rid of negetive number
-        for(auto& i : nums) if(i<0) i = 0;
-        // in-place hashing 
-        for(int i : nums){
-            i = abs(i);
-            if(isSafe(i,n)){
-                if(nums[i-1] == 0){
-                    nums[i-1] = -(n+1);
-                }else{
-                    nums[i-1] = -abs(nums[i-1]);
-                }
-            }
-        }
-        // find ans
-        int ans =  1;
-        for(; ans<=n; ans++) if(nums[ans-1] >= 0) return ans;
-        return ans;
-    }
+    return ans;
 };
