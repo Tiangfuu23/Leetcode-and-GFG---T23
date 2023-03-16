a60e981aa5380592041482f93df7b059ead3c967
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-16 11:49:45 +0700

    Time: 119 ms (17.93%), Space: 57 MB (8.92%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,4 +1,4 @@
-// time O(n) space O(n)
+// time O(n) - worse case: O(n^2) space O(n)
 // note that ans belongs to [1, len(nums)+1]; // worse case : stricly increasing order
 class Solution {
 public:
@@ -8,7 +8,7 @@ public:
         for(int i = 0; i<n; i++){
             st.insert(nums[i]);
         }
-        for(; ans<=n+1; ans++) if(st.find(ans) == st.end()) return ans;
+        for(; ans<=n; ans++) if(st.find(ans) == st.end()) return ans;
         return ans;
     }
 };
