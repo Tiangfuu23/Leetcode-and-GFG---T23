e16f8f05fe563caf9097e24111829bd05eddbf64
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-24 20:41:57 +0700

    Time: 6 ms (28.76%), Space: 15.9 MB (32.82%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // gerateing
+    void gen(int k, int n, vector<vector<int>>& res, vector<int> temp, vector<int>& nums){
+        if(k==n){
+            // goal
+            res.push_back(temp);
+        }else{
+            //choice - no constraint
+            gen(k+1,n,res,temp,nums);
+            temp.push_back(nums[k]);
+            gen(k+1,n,res,temp,nums);
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> res;
+        int n = nums.size(), k = 0;
+        gen(k,n,res,temp,nums);
+        return res;
+    }
+};
