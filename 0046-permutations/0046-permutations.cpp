6b4a6d73ffad2fe725df791f9b65c395f9d2771e
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-03-28 16:20:59 +0700

    Time: 0 ms (100.00%), Space: 7.7 MB (55.33%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,17 @@
 class Solution {
-private:
-    vector<vector<int>> res;
-    vector<int> permutation;
-    unordered_set<int> chosen;
 public:
-    void search(vector<int>& nums){
-        int n = nums.size();
-        if(permutation.size() == n){
-            // goal
-            res.push_back(permutation);
-        }else{
-            for(int i = 0; i<n; i++){
-                int choice = nums[i];
-                if(chosen.find(choice) != chosen.end()) continue;
-                permutation.push_back(choice);
-                chosen.insert(choice);
-                search(nums);
-                chosen.erase(choice);
-                permutation.pop_back();
-            }
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        search(nums);
+        int n = nums.size();
+        vector<int> h(n);
+        vector<vector<int>> res;
+        vector<int> temp(n);
+        for(int i=0; i<n; i++) h[i] = i;
+        do{
+            for(int i = 0; i<n; i++){
+                temp[i] = nums[h[i]];
+            }
+            res.push_back(temp);
+        }while(next_permutation(h.begin(), h.end()));
         return res;
     }
 };
