d4d50972c4e1bf1738d2021de15899f01c05d97d
Tiangfuu23 <95488141+Tiangfuu23@users.noreply.github.com>
2023-04-10 22:46:35 +0700

    Time: 3 ms (95.91%), Space: 7.7 MB (89.97%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,12 @@
+// let y/2 = x, so no.of set bits y - no.of set bits x <= 1;
+// subproblem : no.of set bits i -> arr[i];
+// relation arr[i] = arr[i/2] +  
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        for(int i = 0; i<=n; i++){
-            int temp = i,cnt = 0;
-            while(temp!=0){
-                if(temp&1) cnt++;
-                temp/=2;
-            }
-            ans[i] = cnt;
+        vector<int> ans(n+1,0);
+        for(int i = 1; i<=n; i++){
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
