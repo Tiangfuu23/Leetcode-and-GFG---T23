660e579ea33a977f664ea213ec7b5e549108b0d4
Tai Nguyen <95488141+Tiangfuu23@users.noreply.github.com>
2024-08-03 14:41:15 +0700

    Time: 104 ms (73.22%), Space: 47.6 MB (13.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        const int NEED_TO_REMOVED = 2301;
+        int NEED_TO_REMOVED = 2301;
         int l = 0, n = nums.Length, cnt = 0;
         for(int i = 2; i < n; i++){
             if(nums[i] == nums[i-1] && nums[i] == nums[i-2]){
