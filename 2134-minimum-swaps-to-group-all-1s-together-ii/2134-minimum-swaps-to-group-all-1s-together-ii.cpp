a4e81bc2a25cb074e11bfe19cdc382973e33de52
Tai Nguyen <95488141+Tiangfuu23@users.noreply.github.com>
2024-08-03 09:53:15 +0700

    Time: 74 ms (32.11%), Space: 83.1 MB (47.58%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -5,15 +5,13 @@ public:
         for(int num : nums) if(num) windowSize++;
         int cntOne = 0, max = 0;
         int l = 0, r = 0;
-        while(r < windowSize) {
-            if(nums[r]) cntOne++;
-            r++;
-        };
-        while(r < 2*n){
-            if(nums[r%n]) cntOne++;
-            if(nums[l%n]) cntOne--;
-            max = fmax(max,cntOne);
-            l++; r++;
+        for(int r = 0; r < 2*n; r++){
+            cntOne += nums[r%n];
+            if(r-l+1 > windowSize){
+                cntOne-= nums[l%n];
+                l+=1;
+            }
+            max = fmax(cntOne, max);
         }
         return windowSize - max;
     }
