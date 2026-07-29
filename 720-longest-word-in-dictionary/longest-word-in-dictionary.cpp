ed21eb38addf0ce6f4abd638f6e5438f44d64830
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-07-29 23:02:20 +0700

    Time: 20 ms (65.80%) | Memory: 36.7 MB (18.49%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -1,27 +1,47 @@
+const int k = 26;
+class Trie{
+    public:
+        Trie* children[k];
+        bool endOfWord;
+
+        Trie(){
+            endOfWord = false;
+            for(int i = 0; i < k; i++)
+                children[i] = nullptr;
+        }
+};
+
+void insert(Trie* root, const string& str){
+    Trie* currentNode = root;
+    for(char c : str){
+        int charValue = c - 'a';
+        if(currentNode->children[charValue] == nullptr){
+            currentNode->children[charValue] = new Trie();
+        }
+        currentNode = currentNode->children[charValue];
+    }
+    currentNode->endOfWord = true;
+}
+
+void dfs(Trie* root, string str, string& ans){
+    for(int i = 0; i < k; i++){
+        if(root->children[i] != nullptr && root->children[i]->endOfWord){
+            string temp = str + char('a' + i); 
+            if(temp.size() > ans.size() || (temp.size() == ans.size() && temp < ans))
+                ans = temp;
+            dfs(root->children[i], temp, ans);
+        }
+    }
+}
+
 class Solution {
 public:
     string longestWord(vector<string>& words) {
-        // Brutal force
-        unordered_set<string> st;
-        for(string str: words)
-            st.insert(str);
-        
+        Trie* root = new Trie();
+        for(string str : words)
+            insert(root, str);
         string ans = "";
-        for(string str: words){
-            string pref = "";
-            bool isGood = true;
-            for(char c : str){
-                pref += c;
-                if(!st.count(pref)){
-                    isGood = false;
-                    break;
-                }
-            }
-
-            if(isGood && (pref.size() > ans.size() || (pref.size() == ans.size() && pref < ans)))
-                ans = str;
-        }
-
+        dfs(root, "", ans);
         return ans;
     }
 };
