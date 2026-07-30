74bba20dccfbe69153dbbdd38d5b18ba491ac6a7
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-07-30 09:23:21 +0700

    Time: 20 ms (66.20%) | Memory: 33 MB (55.14%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -1,47 +1,44 @@
-const int k = 26;
-class Trie{
-    public:
-        Trie* children[k];
-        bool endOfWord;
-
-        Trie(){
-            endOfWord = false;
-            for(int i = 0; i < k; i++)
-                children[i] = nullptr;
-        }
-};
-
-void insert(Trie* root, const string& str){
-    Trie* currentNode = root;
-    for(char c : str){
-        int charValue = c - 'a';
-        if(currentNode->children[charValue] == nullptr){
-            currentNode->children[charValue] = new Trie();
-        }
-        currentNode = currentNode->children[charValue];
-    }
-    currentNode->endOfWord = true;
-}
-
-void dfs(Trie* root, string str, string& ans){
-    for(int i = 0; i < k; i++){
-        if(root->children[i] != nullptr && root->children[i]->endOfWord){
-            string temp = str + char('a' + i); 
-            if(temp.size() > ans.size() || (temp.size() == ans.size() && temp < ans))
-                ans = temp;
-            dfs(root->children[i], temp, ans);
-        }
-    }
-}
-
 class Solution {
 public:
+    void dfs(int currentNode, vector<vector<int>>& next,
+             vector<bool>& isEndOfWord, const string& currentString,
+             string& ans) {
+        if (isEndOfWord[currentNode] && currentString.size() > ans.size()) {
+            ans = currentString;
+        }
+
+        for (int v = 0; v < next[currentNode].size(); v++) {
+            int temp = next[currentNode][v];
+            if (temp != 0 && isEndOfWord[temp]) {
+                // cout << currentString << endl;
+                dfs(next[currentNode][v], next, isEndOfWord,
+                    currentString + char(v + 'a'), ans);
+            }
+        }
+    }
+
     string longestWord(vector<string>& words) {
-        Trie* root = new Trie();
-        for(string str : words)
-            insert(root, str);
+        vector<vector<int>> next(1, vector<int>(26, 0));
+        vector<bool> isEndOfWord(1, false);
+        int rowCounter = 1;
+
+        for (string str : words) {
+            int currentNode = 0;
+            for (char c : str) {
+                int charValue = c - 'a';
+                if (next[currentNode][charValue] == 0) {
+                    // insert
+                    next.push_back(vector<int>(26, 0));
+                    next[currentNode][charValue] = rowCounter++;
+                    isEndOfWord.push_back(false);
+                }
+                currentNode = next[currentNode][charValue];
+            }
+            isEndOfWord[currentNode] = true;
+        }
+
         string ans = "";
-        dfs(root, "", ans);
+        dfs(0, next, isEndOfWord, "", ans);
         return ans;
     }
 };
