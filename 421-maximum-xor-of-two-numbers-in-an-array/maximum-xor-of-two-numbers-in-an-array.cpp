3d5a05b8902cd988c30fb247a67a4c0a7b0ff6f3
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-08-03 09:19:27 +0700

    Time: 1168 ms (12.04%) | Memory: 457.2 MB (5.03%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,61 +1,34 @@
-const int k = 2;
-const int bit_length = 30;
-class Trie {
-    public:
-        Trie* children[k];
-        bool isEnd;
-        
-        Trie(){
-            isEnd = false;
-            for(int i = 0; i<k; i++){
-                children[i] = nullptr;
-            }
-        }
-};
-
 class Solution {
 public:
-    // string conver2BinaryFormat(int n){ // O(logn)
-    //     string b = "";
-    //     while(n != 0){
-    //         b = to_string(n % 2) + b;
-    //         n = n/2; 
-    //     }
-
-    //     return b;
-    // }
-
-    void insert(Trie* root, const int& num){
-        Trie* current_node = root;
-        for(int i = bit_length; i >= 0; i--){
-            int i_bit = 1 & (num >> i);
-            if(current_node->children[i_bit] == nullptr){
-                current_node->children[i_bit] = new Trie();
-            }
-            current_node = current_node->children[i_bit];
-        }
-
-        current_node->isEnd = true;
-    }
-
     int findMaximumXOR(vector<int>& nums) {
-        // build trie
-        Trie* root = new Trie();
+        int bit_len = 30;
+        int row_idx_count = 0;
+        vector<vector<int>> trie(1, vector<int>(2, -1));
+        // Build trie
         for(int n : nums){
-            insert(root, n);
+            int current_node = 0;
+            for(int i = bit_len; i>=0; i--){
+                int i_bit = (n >> i) & 1;
+                if(trie[current_node][i_bit] == -1){
+                    trie[current_node][i_bit] = ++row_idx_count;
+                    trie.push_back(vector<int>(2, -1));
+                }
+                current_node = trie[current_node][i_bit];
+            }
         }
 
+        //
         int ans = 0;
         for(int n : nums){
-            Trie* current_node = root;
+            int current_node = 0;
             int temp = 0;
-            for(int i = bit_length; i >= 0; i--){
-                int i_bit = 1 & (n >> i);
-                if(current_node->children[1-i_bit] != nullptr){
-                    temp = temp | (1<<i);
-                    current_node = current_node->children[1-i_bit];
+            for(int i = bit_len; i >= 0; i--){
+                int i_bit = (n >> i) & 1;
+                if(trie[current_node][1-i_bit] != -1){
+                    temp = temp | (1 << i);
+                    current_node = trie[current_node][1-i_bit];
                 }else{
-                    current_node = current_node->children[i_bit];
+                    current_node = trie[current_node][i_bit];
                 }
             }
             ans = max(ans, temp);
