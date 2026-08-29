16024b8ce94a659c9699770e13644efa84bd9843
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-08-29 11:24:45 +0700

    Time: 8 ms (99.27%) | Memory: 20.4 MB (5.07%) - LeetSync

---
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    vector<pair<int ,int>> compress(vector<int>& nums){
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        unordered_map<int, pair<int, int>> mp; // map value to index
+        int counter = 0;
+        for(int i = 0; i<temp.size(); i++){
+            if(mp.find(temp[i]) == mp.end()){
+                mp.insert({temp[i], make_pair(counter++, 1)});
+            }else{
+                mp[temp[i]].second++;
+            }
+        }
+        vector<pair<int,int>> compressed_array;
+        for(int& n : nums){
+            compressed_array.push_back(mp[n]);
+        }
+
+        return compressed_array;
+    }
+
+    pair<int, int> queryMax(vector<pair<int, int>>& tree, int v, int vl, int vr, int l, int r){
+        if(vr < l || vl > r)
+            return {0,0};
+        if(vl >= l && vr <= r){
+            return tree[v];
+        }
+        int m = (vl+vr)/2;
+
+        pair<int, int> pl = queryMax(tree, v*2, vl, m, l ,r);
+        pair<int, int> pr = queryMax(tree, v*2+1, m+1, vr, l ,r);
+
+        if(pl.first < pr.first){
+            return pr;
+        }else if(pl.first > pr.first){
+            return pl;
+        }else{
+            return make_pair(pl.first, pl.second  + pr.second);
+        }
+    }
+
+    void update(vector<pair<int, int>>& tree, int v, int vl, int vr, int pos, pair<int, int> val){
+        if(vl == vr){
+            if(tree[v].first == val.first){
+                tree[v].second += val.second;
+            }else{
+                tree[v] = val;
+            }
+            return;
+        }
+        int m = (vl+vr)/2;
+        if(pos <= m){
+            update(tree, v*2, vl, m, pos, val);
+        }else{
+            update(tree, v*2+1, m+1, vr, pos, val);
+        }
+
+        tree[v].first = max(tree[v*2].first, tree[v*2+1].first);
+
+        if(tree[v*2].first == tree[v*2+1].first){
+            tree[v].second = tree[v*2].second + tree[v*2+1].second;
+        }else{
+            tree[v].second = (tree[v].first == tree[v*2].first) ? tree[v*2].second : tree[v*2+1].second;
+        }
+    }
+
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> arr = compress(nums);
+        vector<pair<int, int>> tree(4*n);
+        for(auto& pr : arr){
+            // find max range 0 -> i - 1;
+            int i = pr.first; // value
+            int freq = pr.second; // frequency
+            pair<int, int> p = (i == 0 ? make_pair(0, 0) : queryMax(tree, 1, 0, n-1, 0, i-1));
+            p.first = p.first + 1;
+            p.second = p.second != 0 ? p.second : 1;
+            // update
+            update(tree, 1, 0, n-1, i, p);
+        }
+
+        return tree[1].second;
+    }
+};
