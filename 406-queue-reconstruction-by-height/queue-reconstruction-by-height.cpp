fd937d5e62012a160f9a9eb9869cbb34f763c018
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-08 16:39:38 +0700

    Time: 3 ms (96.58%) | Memory: 16.2 MB (39.02%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,39 +1,32 @@
 int inf = 1e6+1;
 class Solution {
 public:
-    void init(vector<pair<int ,int>>& tree, int v, int vl, int vr){
-        tree[v] = {inf, vr - vl + 1};
-        if(vl == vr) return;
+    void build(vector<int>& tree, int v, int vl, int vr){
+        if(vl == vr) {
+            tree[v] = 1;
+            return;
+        };
         
         int m = (vl + vr)/2;
-        init(tree, v*2, vl, m);
-        init(tree, v*2+1, m + 1, vr);
-    }
-    pair<int, int> chooseBest(pair<int, int>& pl, pair<int, int>& pr){
-        if(pl.first < pr.first) return pr;
-        
-        if(pl.first > pr.first) return pl;
-
-        return {pl.first, pl.second + pr.second};
+        build(tree, v*2, vl, m);
+        build(tree, v*2+1, m + 1, vr);
+        tree[v] = tree[v*2] + tree[v*2+1];
     }
 
-    pair<int, int> findMax(vector<pair<int, int>>& tree, int v, int vl, int vr, int l, int r){
-        if(vr < l || vl > r){
-            return {-1, 0}; // 
+    // same as find k-th min element ?
+    int findKthEmpty(vector<int>& tree, int v, int vl, int vr, int k){
+        if(vl == vr){
+            return vl;
         }
-
-        if(vl >= l && vr <= r){
-            return tree[v];
+        int m = (vl + vr) / 2;
+        if(k <= tree[v*2]){
+            return findKthEmpty(tree, v*2, vl, m, k);
+        }else{
+            return findKthEmpty(tree, v*2 + 1, m+1, vr, k - tree[v*2]);
         }
-
-        int m = (vl+vr)/2;
-        pair<int, int> pl = findMax(tree, v*2, vl, m, l, r);
-        pair<int, int> pr = findMax(tree, v*2 + 1, m+1, vr, l, r);
-    
-        return chooseBest(pl, pr);
     }
 
-    void update(vector<pair<int, int>>& tree, int v, int vl, int vr, int pos, pair<int, int> val){
+    void update(vector<int>& tree, int v, int vl, int vr, int pos, int  val){
         if(vl == vr){
             tree[v] = val;
             return;
@@ -47,10 +40,9 @@ public:
             update(tree, v*2+1, m + 1, vr, pos, val);
         }
 
-        pair<int, int> p = chooseBest(tree[v*2], tree[v*2 + 1]);
-        tree[v] = p;
+        tree[v] = tree[v*2] + tree[v*2+1];
     }
-
+    // time = n*logn + n*logn
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         int n = people.size();
         sort(people.begin(), people.end(), [](vector<int>& p1, vector<int>& p2){
@@ -60,32 +52,16 @@ public:
             return p1[0] < p2[0];
         });
         
-        vector<pair<int, int>> tree(4*n); // max value, no.of time its appear
-        init(tree, 1, 0, n-1);
+        vector<int> tree(4*n);
+        build(tree, 1, 0, n-1);
         
         vector<vector<int>> ans(n, vector<int>(2));
 
-        unordered_set<int> isTaken;
-
         for(vector<int>& p : people){
-            int i = p[1];
-
-            for(; i < n; i++){
-                if(isTaken.find(i) != isTaken.end()) continue;
-                // check range [0, i-1]
-                pair<int, int> q = findMax(tree, 1, 0, n-1, 0, i-1);
-                
-                bool isFound = (q.first >= p[0] && q.second == p[1]) || (q.first < p[0] && p[1] == 0);
-
-                if(isFound){
-                    isTaken.insert(i);
-                    update(tree, 1, 0 , n-1, i, {p[0], 1});
-                    break;
-                }
-            }
-            if(i < n){
-                ans[i] = p;
-            }
+            int pos = findKthEmpty(tree, 1, 0, n-1, p[1] + 1);
+            update(tree, 1, 0, n - 1, pos, 0);
+            // ans.push_back({p[0], p[1], pos});
+            ans[pos] = p;
         }
 
         return ans;
