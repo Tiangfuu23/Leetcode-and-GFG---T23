5ee1c00f51708a1f07d8701d15658870d5bb72bc
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-08 15:49:52 +0700

    Time: 69 ms (6.74%) | Memory: 17.1 MB (11.69%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,28 +1,25 @@
-int inf = -1;
-// tuple<int, int, int> naturalValue = {inf, 1, };
+int inf = 1e6+1;
 class Solution {
 public:
-    void init(vector<tuple<int ,int, int>>& tree, int v, int vl, int vr){
-        tree[v] = {inf, vr - vl + 1, vr - vl + 1};
+    void init(vector<pair<int ,int>>& tree, int v, int vl, int vr){
+        tree[v] = {inf, vr - vl + 1};
         if(vl == vr) return;
         
         int m = (vl + vr)/2;
         init(tree, v*2, vl, m);
         init(tree, v*2+1, m + 1, vr);
     }
-    tuple<int, int, int> chooseBest(tuple<int, int, int>& a, tuple<int, int, int>& b){
-        auto [a1, a2, a3] = a;
-        auto [b1, b2, b3] = b;
-        if(a1 < b1) return {b1, b2, a3 + b3};
+    pair<int, int> chooseBest(pair<int, int>& pl, pair<int, int>& pr){
+        if(pl.first < pr.first) return pr;
         
-        if(a1 > b1) return {a1, a2, a3 + b3};
+        if(pl.first > pr.first) return pl;
 
-        return {a1, a2 + b2, a3 + b3};
+        return {pl.first, pl.second + pr.second};
     }
 
-    tuple<int, int, int> findMax(vector<tuple<int, int, int>>& tree, int v, int vl, int vr, int l, int r){
+    pair<int, int> findMax(vector<pair<int, int>>& tree, int v, int vl, int vr, int l, int r){
         if(vr < l || vl > r){
-            return {inf - 1, 0, 0}; // 
+            return {-1, 0}; // 
         }
 
         if(vl >= l && vr <= r){
@@ -30,13 +27,13 @@ public:
         }
 
         int m = (vl+vr)/2;
-        auto pl = findMax(tree, v*2, vl, m, l, r);
-        auto pr = findMax(tree, v*2 + 1, m+1, vr, l, r);
+        pair<int, int> pl = findMax(tree, v*2, vl, m, l, r);
+        pair<int, int> pr = findMax(tree, v*2 + 1, m+1, vr, l, r);
     
         return chooseBest(pl, pr);
     }
 
-    void update(vector<tuple<int, int, int>>& tree, int v, int vl, int vr, int pos, tuple<int, int, int> val){
+    void update(vector<pair<int, int>>& tree, int v, int vl, int vr, int pos, pair<int, int> val){
         if(vl == vr){
             tree[v] = val;
             return;
@@ -50,26 +47,21 @@ public:
             update(tree, v*2+1, m + 1, vr, pos, val);
         }
 
-        auto  p = chooseBest(tree[v*2], tree[v*2 + 1]);
+        pair<int, int> p = chooseBest(tree[v*2], tree[v*2 + 1]);
         tree[v] = p;
     }
 
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         int n = people.size();
         sort(people.begin(), people.end(), [](vector<int>& p1, vector<int>& p2){
-            if(p1[0] == p2[0]) return p1[1] < p2[1];
+            if(p1[0] == p2[0]) return p1[1] > p2[1];
             return p1[0] < p2[0];
         });
         
-        vector<tuple<int, int, int>> tree(4*n); // max value, no.of time its appear
+        vector<pair<int, int>> tree(4*n); // max value, no.of time its appear
         init(tree, 1, 0, n-1);
         
         vector<vector<int>> ans(n, vector<int>(2));
-        
-        // for(auto [a,b,c] : tree){
-        //     ans.push_back({a,b,c});
-        // }        
-        // return ans;
 
         unordered_set<int> isTaken;
 
@@ -79,29 +71,31 @@ public:
             for(; i < n; i++){
                 if(isTaken.find(i) != isTaken.end()) continue;
                 // check range [0, i-1]
-                auto [a, b ,c] = findMax(tree, 1, 0, n-1, 0, i-1);
+                pair<int, int> q = findMax(tree, 1, 0, n-1, 0, i-1);
                 
-                bool isFound = (a >= p[0] && b + c == p[1]) || (a < p[0] && c == p[1]);
+                bool isFound = (q.first >= p[0] && q.second == p[1]) || (q.first < p[0] && p[1] == 0);
 
-                // if(p[0] == 5 && p[1] == 2 && i == 2){
-                //     ans.push_back({a,b,c});
-                //     return ans;
+                // if(p[0] == 6 && p[1] == 1 && i == 3){
+                //     // vector<int> temp = {q.first, q.second};
+                //     // ans.push_back(temp);
+                //     // return ans;
 
-                //         //     for(auto p : tree){
-                //         //     ans.push_back({p.first, p.second});
-                //         // }
+                //             for(auto p : tree){
+                //             ans.push_back({p.first, p.second});
+                //         }
                         
-                //         // return ans;
+                //         return ans;
                 // }
 
-                    
                 if(isFound){
                     isTaken.insert(i);
-                    update(tree, 1, 0 , n-1, i, {p[0], 1, 0});
+                    update(tree, 1, 0 , n-1, i, {p[0], 1});
                     break;
                 }
             }
+            if(i < n){
                 ans[i] = p;
+            }
         }
 
         return ans;
