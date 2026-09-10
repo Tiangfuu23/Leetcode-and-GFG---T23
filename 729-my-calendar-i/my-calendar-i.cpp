3d525bb731349c93a02e2e8e21e5ab0f8e220fe7
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-10 10:15:13 +0700

    Time: 12 ms (82.15%) | Memory: 44.2 MB (29.34%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -2,18 +2,14 @@ class MyCalendar {
 public:
     map<int, int> mp; // end time - start time
     MyCalendar() {
-        mp.clear();
+        // mp.clear();
     }
     
     bool book(int startTime, int endTime) {
-        auto leftCheckEvent = mp.upper_bound(startTime);
-        if(leftCheckEvent != mp.end() && leftCheckEvent->second < endTime) // exist event which end time > current start time
+        auto checkedEvent = mp.upper_bound(startTime);
+        if(checkedEvent != mp.end() && checkedEvent->second < endTime) // exist event which end time > current start time
             return false;
-            
-        // auto rightCheckEvent = mp.lower_bound(endTime);
-        // if(rightCheckEvent != mp.end() && rightCheckEvent->second < endTime)
-        //     return false;
-
+        
         mp.insert({endTime, startTime});
         return true;
     }
