c7eea8741b5db703bbd1998155125b953e115d8c
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-12 08:57:57 +0700

    Time: 7 ms (3.25%) | Memory: 12.3 MB (5.08%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int maxLen = max(a.size(), b.size());
+        string ans = "";
+        int debt = 0;
+        for(int i = 0; i < maxLen; i++){
+            int x = i < a.size() ? toInt(a[a.size() - 1 - i]) : 0;
+            int y = i < b.size() ? toInt(b[b.size() - 1 - i]) : 0;
+            int new_bit = x + y + debt;
+            debt = 0;
+            // return to_string(x) + " || " + to_string(y);
+            if(new_bit > 1) {
+                new_bit = new_bit - 2;
+                debt = 1;
+            }
+            ans = to_string(new_bit) + ans;
+        }
+        if(debt == 1) ans = "1" + ans;
+        return  ans;
+    }
+
+    int toInt(char i){
+        return i == '0' ? 0 : 1;
+    }
+};
