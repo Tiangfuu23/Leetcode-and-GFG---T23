c0c0f5a791d8a5e28a3b697ae39480b2775b32d7
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-12 14:25:49 +0700

    Time: 63 ms (62.91%) | Memory: 241.1 MB (80.47%) - LeetSync

--- 1294-range-frequency-queries/range-frequency-queries.cpp
+++ 1294-range-frequency-queries/range-frequency-queries.cpp
@@ -2,22 +2,28 @@ class RangeFreqQuery {
 public:
     unordered_map<int, vector<int>> mp; // value -> stored indices
     RangeFreqQuery(vector<int>& arr) {
-        for(int i = 0; i<arr.size(); i++){
+        for (int i = 0; i < arr.size(); i++) {
             mp[arr[i]].push_back(i);
         }
     }
-    
-    int query(int left, int right, int value) {
-        if(mp.find(value) == mp.end())
-            return 0;
-        
-        vector<int>& arr = mp[value];
-        auto li = lower_bound(arr.begin(), arr.end(), left);
-        auto ri = upper_bound(arr.begin(), arr.end(), right);
-        int l = li - arr.begin();
-        int r = ri - arr.begin() - 1;
 
-        return r - l + 1;
+    int query(int left, int right, int value) {
+        // if(mp.find(value) == mp.end())
+        //     return 0;
+
+        // vector<int>& arr = mp[value];
+        // auto li = lower_bound(arr.begin(), arr.end(), left);
+        // auto ri = upper_bound(arr.begin(), arr.end(), right);
+        // int l = li - arr.begin();
+        // int r = ri - arr.begin() - 1;
+
+        // return r - l + 1;
+        auto it = mp.find(value);
+        if (it == mp.end())
+            return 0;
+        vector<int>& v = it->second;
+        return upper_bound(v.begin(), v.end(), right) -
+               lower_bound(v.begin(), v.end(), left);
     }
 };
 
