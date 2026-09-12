bfda48d94658c8d4ee938492c232a7aaa2a8c856
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-12 14:20:41 +0700

    Time: 75 ms (39.71%) | Memory: 241.1 MB (95.28%) - LeetSync

---
+++ 1294-range-frequency-queries/range-frequency-queries.cpp
@@ -0,0 +1,28 @@
+class RangeFreqQuery {
+public:
+    unordered_map<int, vector<int>> mp; // value -> stored indices
+    RangeFreqQuery(vector<int>& arr) {
+        for(int i = 0; i<arr.size(); i++){
+            mp[arr[i]].push_back(i);
+        }
+    }
+    
+    int query(int left, int right, int value) {
+        if(mp.find(value) == mp.end())
+            return 0;
+        
+        vector<int>& arr = mp[value];
+        auto li = lower_bound(arr.begin(), arr.end(), left);
+        auto ri = upper_bound(arr.begin(), arr.end(), right);
+        int l = li - arr.begin();
+        int r = ri - arr.begin() - 1;
+
+        return r - l + 1;
+    }
+};
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
