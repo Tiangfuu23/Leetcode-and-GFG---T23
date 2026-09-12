caddf2269efc1d9195f59a04cfbeb5e84df3f923
Trần Anh Tài <95488141+Tiangfuu23@users.noreply.github.com>
2026-09-12 16:46:13 +0700

    Time: 79 ms (29.97%) | Memory: 172.1 MB (15.82%) - LeetSync

--- 2512-longest-uploaded-prefix/longest-uploaded-prefix.cpp
+++ 2512-longest-uploaded-prefix/longest-uploaded-prefix.cpp
@@ -1,20 +1,51 @@
+// Time complexity: Query: O(1)
+// Time complexity: Upload: O(logn)
+
+// Find smallest video which is not updated
 class LUPrefix {
 public:
-    unordered_set<int> st;
-    int ans = 0;
+    int inf = 1e5 + 1;
+    vector<int> tree;
+    int n;
     LUPrefix(int n) {
-        
+        tree = vector<int>(4*n);
+        this->n = n;
+        build(tree, 1, 0, n-1);
     }
-    
-    void upload(int video) {
-        st.insert(video);
-        while(st.find(ans + 1) != st.end()){
-            ans++;
+    void build(vector<int>& tree, int v, int vl, int vr){
+        if(vl == vr){
+            tree[v] = vl;
+            return;
         }
+
+        int m = (vl + vr)/2;
+        build(tree, v*2, vl, m);
+        build(tree, v*2+1, m+1, vr);
+
+        tree[v] = tree[v*2];
+    }
+    void update(vector<int>& tree, int v, int vl, int vr, int pos){
+        if(vl == vr){
+            tree[v] = inf;
+            return;
+        }
+
+        int mid = (vl + vr) / 2;
+        if(pos <= mid){
+            update(tree, v*2, vl, mid, pos);
+        }else{
+            update(tree, v*2+1, mid+1, vr, pos);
+        }
+        tree[v] = min(tree[v*2], tree[v*2+1]);
+    }
+
+    void upload(int video) {
+        // int videoIdx = video - 1
+        update(tree, 1, 0, n-1, video - 1);
     }
     
     int longest() {
-        return ans;
+        return min(tree[1], n);
     }
 };
 
